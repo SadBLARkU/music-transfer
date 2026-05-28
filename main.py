7aadb4179ff3b4fc30721244124fc2e5fd17605d
SadBLARkU <boris20tikhomirov@gmail.com>
2026-05-28 19:10:50 +0300

    final step 1

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 print("Hi, This is start of my first pet-project")
 print("test of commiting by button")
 print("Second test of commiting by commands")
-<<<<<<< HEAD
 print("wtf")
-=======
->>>>>>> 3d12064b07fe186c50600b47a62a0415500ec593
